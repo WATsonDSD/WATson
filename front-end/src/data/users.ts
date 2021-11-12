4d00eb90e27e947d706c1675aefd41998392f43d
Cem <Cebeci>
2021-11-12 13:35:41 +0100

    implemented `createUser`.

--- front-end/src/data/users.ts
+++ front-end/src/data/users.ts
@@ -1,6 +1,6 @@
 import { Project } from '.';
 import { Users } from './dummyData';
-import { User, UserID } from './types';
+import { Role, User, UserID } from './types';
 
 export async function findUserById(id: UserID): Promise<User> {
   const res = Users[id];
@@ -17,4 +17,28 @@ export async function getUsersOfProject(project: Project): Promise<User[]> {
   return Promise.all(
     project.users.map((id) => findUserById(id)),
   );
+}
+
+/**
+ * Creates a new `User`.
+ * @returns The newly created user's `id`, determined by the backend.
+ * 
+ * @example
+ * const efflamsId = await createUser('Efflam Simone', 'efflam@watson.com', 'annotator');
+ * // returns 'Efflam Simone'
+ * getUserById(efflamsId).then(user => user.name);
+ */
+export async function createUser(name: string, email: string, role: Role): Promise<UserID> {
+  // unique Id's, should work for now.
+  const id = new Date().toISOString();
+
+  Users[id] = {
+    id,
+    name,
+    email,
+    role,
+    projects: {}, // a new user has no projects.
+  };
+
+  return id;
 }
