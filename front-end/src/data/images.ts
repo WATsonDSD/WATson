993605226067b98279a140f92be5cc8bedc47269
Cem <Cebeci>
2021-11-12 22:58:08 +0100

    implemented 'saveAnnotation'

--- front-end/src/data/images.ts
+++ front-end/src/data/images.ts
@@ -1,6 +1,7 @@
 import { Images } from './dummyData';
 import {
-  ImageID, Image, ProjectID, UserID, findUserById, findProjectById,
+  ImageID, Image, ProjectID, UserID, findUserById,
+  findProjectById, Annotation, LandmarkSpecification,
 } from '.';
 
 /*
@@ -33,4 +34,45 @@ export async function getImages(
   }
   const project = await findProjectById(projectID);
   return Promise.all(project.images[status].map((id) => findImageById(id)));
+}
+
+/**
+ * Determines whether `annotation` is valid for the `specification`. 
+ */
+function fitsSpecification(annotation: Annotation, specification:LandmarkSpecification): boolean {
+  let valid = true;
+  specification.forEach((landmark) => { if (!annotation[landmark]) valid = false; });
+  return valid;
+}
+
+/**
+ * Saves the annotation and associates it with the image.
+ * Also moves the image from 'toAnnotate' to 'toVerify'.
+ * ! This function does NOT assign a verifier (yet).
+ * @param annotation Has to be complete and has to match the project's `LandmarkSpecification`.
+ * @param imageId The image the annotation will be associated to.
+ * @param projectId The project that includes the image.
+ */
+export async function saveAnnotation(
+  annotation: Annotation,
+  imageId: ImageID,
+  projectId: ProjectID,
+): Promise<void> {
+  const project = await findProjectById(projectId);
+  // check if the annotation is valid.
+  if (!fitsSpecification(annotation, project.landmarks)) {
+    throw Error("The annotation does not fit the project's specification");
+  }
+
+  // check if the image is waiting to be annotated.
+  const imageIndex = project.images.toAnnotate.findIndex((id) => id === projectId);
+  if (imageIndex < 0) { throw Error('The image does not exist the the project'); }
+
+  // save the annotation
+  const image = await findImageById(imageId);
+  image.annotation = annotation;
+
+  // move to toVerify
+  project.images.toAnnotate.splice(imageIndex, 1); // remove from toAnnotate.
+  project.images.toVerify.push(imageId);
 }
