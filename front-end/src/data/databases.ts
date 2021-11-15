ed8a10f6ae2ca415cc7106213f1f5f5a0d41ac3c
Cem <Cebeci>
2021-11-15 17:17:29 +0100

    types on databases.

--- front-end/src/data/databases.ts
+++ front-end/src/data/databases.ts
@@ -1,5 +1,6 @@
 import PouchDB from 'pouchdb';
+import { Image, Project, User } from '.';
 
-export const usersDB = new PouchDB('http://admin:admin@127.0.0.1:5984/users');
-export const projectsDB = new PouchDB('http://admin:admin@127.0.0.1:5984/projects');
-export const imagesDB = new PouchDB('http://admin:admin@127.0.0.1:5984/images');
+export const usersDB = new PouchDB<User>('http://admin:admin@127.0.0.1:5984/users');
+export const projectsDB = new PouchDB<Project>('http://admin:admin@127.0.0.1:5984/projects');
+export const imagesDB = new PouchDB<Image>('http://admin:admin@127.0.0.1:5984/images');
