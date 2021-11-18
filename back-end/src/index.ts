3491a9b6f0cedfe81e9cc7c6839034eb03c8dd4c
Rahman Khalaf <screen.generation.soft@gmail.com>
2021-11-18 15:48:36 +0100

    Connected the application to Cloudant

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,4 +1,33 @@
-console.log("Hello, world!");
-console.log("He's Alive!");
+import fastify from "fastify";
+import vcap from "./config/vcap-local.json";
+import { CloudantV1 } from "@ibm-cloud/cloudant";
 
-export{}
+// ! There could be a cleaner way to set up environment variables
+process.env.CLOUDANT_URL = vcap.services.cloudantNoSQLDB.credentials.url;
+process.env.CLOUDANT_APIKEY = vcap.services.cloudantNoSQLDB.credentials.apikey;
+
+const server = fastify()
+
+const cloudant = CloudantV1.newInstance({
+  serviceName: "CLOUDANT",
+});
+
+// Lists all the databases on Cloudant
+// cloudant.getAllDbs().then((response) => {
+//   console.log(response.result);
+// });
+
+// An example with fastify
+server.get("/", async (request, reply) => {
+  cloudant.getAllDbs().then((response) => {
+      reply.send(response.result);
+  });
+});
+
+server.listen(8080, (error, address) => {
+  if (error) {
+    console.log(error);
+    process.exit(1);
+  }
+  console.log(`Server listening at ${address}`);
+});
