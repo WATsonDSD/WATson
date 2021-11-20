e35fccd243f58b719e2246d52dcfeb64622925b4
Efflam <efflam.simon@etu.univ-nantes.fr>
2021-11-20 20:08:09 +0100

    Add colors to template depending on landmark state + Rework color system

--- front-end/src/view/components/annotation/AnnoatedImage.tsx
+++ front-end/src/view/components/annotation/AnnoatedImage.tsx
@@ -2,48 +2,36 @@ import React, { useRef, useEffect } from 'react';
 import template from './template.png';
 import { Image } from '../../../data';
 
-const colors: any = { 0: '#FF0000', 1: '#0000FF', 2: '#22AA00' };
-
 // Used for template image, image to annotate and image to verify.
 // Has a background image and some points drawn on it that represents landmarks
 AnnotatedImage.defaultProps = {
   onClick: undefined,
-  highlightedLandmark: undefined,
   hideNonVisible: false,
-  landmarkColor: undefined,
 };
 export default function AnnotatedImage(props: {
   image: Image,
   onClick?: Function,
-  highlightedLandmark?: number,
   hideNonVisible?: boolean,
-  landmarkColor?: Function,
+  landmarkColor: Function,
 }) {
-  const { image, onClick, highlightedLandmark } = props;
-
   const canvasRef = useRef(null);
 
   const draw = (ctx: any) => {
     const backgroundImage = new window.Image();
-    backgroundImage.src = (image.data || template);
+    backgroundImage.src = (props.image.data || template);
     backgroundImage.onload = () => {
       ctx.drawImage(backgroundImage, 0, 0, ctx.canvas.width, ctx.canvas.height);
-      if (image.annotation) {
-        Object.entries(image.annotation).forEach(([id, point]) => {
+      if (props.image.annotation) {
+        Object.entries(props.image.annotation).forEach(([id, point]) => {
           if (props.hideNonVisible && point.z === 0) return;
-          if (props.landmarkColor) {
-            ctx.fillStyle = props.landmarkColor(id);
-          } else {
-            ctx.fillStyle = colors[point.z];
-          }
+
+          const { fill, stroke } = props.landmarkColor(+id);
+          [ctx.fillStyle, ctx.strokeStyle] = [fill || '#00000000', stroke || '#00000000'];
+
           ctx.beginPath();
           ctx.arc(point.x * ctx.canvas.width, point.y * ctx.canvas.height, 5, 0, 2 * Math.PI);
-          ctx.fill();
-          if (highlightedLandmark === +id) {
-            ctx.strokeStyle = '#FFF';
-            ctx.lineWidth = 3;
-            ctx.stroke();
-          }
+          if (fill) ctx.fill();
+          if (stroke) ctx.stroke();
         });
       }
     };
@@ -52,8 +40,11 @@ export default function AnnotatedImage(props: {
   useEffect(() => {
     const canvas = canvasRef.current;
     const context: any = (canvas || { getContext: (s: string) => { throw s; } }).getContext('2d');
+    const { onClick } = props;
     draw(context);
-    if (onClick) context.canvas.onclick = (event: any) => onClick(context, event);
+    if (onClick) {
+      context.canvas.onclick = (event: any) => onClick(context, event);
+    }
   }, [draw]);
 
   return <canvas ref={canvasRef} width="300" height="300" />;
