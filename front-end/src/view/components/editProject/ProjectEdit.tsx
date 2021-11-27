dafbba0576d7187c4730c246a270f7c2a436f093
paultournu <58809314+paultournu@users.noreply.github.com>
2021-11-27 17:44:38 +0100

    Adding Project Page for PM

---
+++ front-end/src/view/components/editProject/ProjectEdit.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import Header from '../shared/layout/Header';
+
+export default function ProjectEdit() {
+  return (<div className="h-full w-full"><Header title="This is ProjectEdit" /></div>);
+}
