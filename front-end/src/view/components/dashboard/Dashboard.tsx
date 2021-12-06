221330f80dd7da30ec4128aa91a900c0dc77ab53
ZiedA <almiazied@live.com>
2021-12-06 21:17:14 +0100

    front-end financial summary page

--- front-end/src/view/components/dashboard/Dashboard.tsx
+++ front-end/src/view/components/dashboard/Dashboard.tsx
@@ -60,7 +60,7 @@ export default function Dashboard() {
   return (
     <div className="min-h-full w-full">
       <div>
-        <Header title="Projects" button={addProjectButton} />
+        <Header title="Projects" button={user !== 'isLoading' && user?.role === 'projectManager' ? addProjectButton : undefined} />
       </div>
 
       <div className="Card min-h-full">
