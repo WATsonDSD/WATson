9f6b855be9a0c84ed311f569fc8611eb36f278ef
paultournu <58809314+paultournu@users.noreply.github.com>
2021-12-09 18:56:57 +0100

    Modals for Profile Settings

--- front-end/src/view/components/shared/menu/UserSettings.tsx
+++ front-end/src/view/components/shared/menu/UserSettings.tsx
@@ -1,33 +1,7 @@
 import React, { Fragment } from 'react';
 import { Menu, Transition } from '@headlessui/react';
 import { User, useUserContext } from '../../../../data';
-
-function openModal(value: boolean, id: string) {
-  const modalOverlay = document.querySelector('#modals');
-  const modal = document.querySelector(id);
-  const modalCl = modal?.classList;
-  const overlayCl = modalOverlay;
-
-  if (value) {
-    console.log(0);
-    overlayCl?.classList.remove('hidden');
-    setTimeout(() => {
-      modalCl?.remove('opacity-0');
-      modalCl?.remove('-translate-y-full');
-      modalCl?.remove('scale-150');
-      modalCl?.remove('hidden');
-    }, 100);
-  } else {
-    console.log(1);
-    modalCl?.add('-translate-y-full');
-    setTimeout(() => {
-      modalCl?.add('opacity-0');
-      modalCl?.add('scale-150');
-      modalCl?.add('hidden');
-    }, 100);
-    setTimeout(() => overlayCl?.classList.add('hidden'), 300);
-  }
-}
+import openModal from '../layout/OpenModal';
 
 function classNames(...classes:any) {
   return classes.filter(Boolean).join(' ');
