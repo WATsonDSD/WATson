b5617d9208b4d6a7b3991679f3f93da6ef37895e
Rahman Khalaf <screen.generation.soft@gmail.com>
2021-12-09 15:46:56 +0100

    Fixed funny bug that made every svg rotate

--- front-end/src/view/components/loading/index.tsx
+++ front-end/src/view/components/loading/index.tsx
@@ -5,7 +5,7 @@ import './index.css';
 export default function Loading() {
   return (
     <div className="flex items-center justify-center w-full h-screen bg-gray-100">
-      <svg viewBox="25 25 50 50">
+      <svg className="loading" viewBox="25 25 50 50">
         <circle cx="50" cy="50" r="20" />
       </svg>
     </div>
