52971c5ab9766b98a002cd694e59eed642180b2b
ZiedA <almiazied@live.com>
2021-12-10 18:40:07 +0100

    card flex direction

--- front-end/src/view/components/project/ProjectAssign.tsx
+++ front-end/src/view/components/project/ProjectAssign.tsx
@@ -28,6 +28,10 @@ export default function ProjectAssign() {
     getUsersOfProject(idProject || '').then((result) => { setProjectUsers(result); });
   }, []);
 
+  const onCancelClick = () => {
+    navigate(Paths.Projects);
+  };
+
   const updateToAnnotate = (newElement: any) => {
     setToAnnotate((prevState) => [...prevState, newElement]);
   };
@@ -84,48 +88,59 @@ export default function ProjectAssign() {
   return (
     <div className="min-h-full w-full">
       <Header title={`Assigning images : ${project?.name ?? ''}`} />
-      <h1 className="ml-2 pl-2 mt-3 text-gray-800 text-2xl font-bold ">Drag and drop images to assign them :</h1>
+      <h1 className="ml-2 pl-2 mt-3 text-gray-800 text-3xl font-bold ">Drag and drop to assign :</h1>
+      <div id="annotation" className="h-1/2 my-5">
+        <h2 className="ml-2 pl-2 mt-3 text-gray-800 text-2xl font-bold ">Annotation :</h2>
 
-      <div className="flex flex-row flex-wrap h-1/2">
-        <div className="rounded-lg shadow-xl bg-gray-50 ">
-          <header className="font-semibold text-sm py-3 px-4">
-            Images to annotate
-          </header>
-          <div className="Pictures grow shrink flex flex-row gap-2">
-            {imagesToAnnotate.map((image) => <ImageDnD key={image.id} type="annotate" data={image.data} id={image.id} />)}
+        <div className="flex flex-row flex-wrap gap-4 h-full">
+          <div className="flex flex-col rounded-lg shadow-xl bg-gray-50 ">
+            <header className="font-semibold text-sm py-3 px-4">
+              Images to annotate
+            </header>
+            <div className="Pictures grow shrink flex flex-col gap-2">
+              {imagesToAnnotate.map((image) => <ImageDnD key={image.id} type="annotate" data={image.data} id={image.id} />)}
+            </div>
           </div>
-        </div>
-        <div className="flex flex-row grow shrink gap-4">
-          { projectUsers.filter((user) => user.role === 'annotator').map((user, index) => (
-            <UserCardDnD key={`${user.id}-annotator`} userId={user.id} accept="annotate" images={toAnnotate.filter((e) => e.user === user.id)} onDrop={(item: any) => handleDrop(index, item, user.id, 'annotate')} />
-          ))}
-        </div>
-      </div>
-      <h1 className="ml-2 pl-2 mt-3 text-gray-800 text-2xl font-bold ">Drag and drop images to assign them :</h1>
-
-      <div className="flex flex-row flex-wrap h-1/2">
-        <div className="rounded-lg shadow-xl bg-gray-50">
-          <header className="font-semibold text-sm py-3 px-4">
-            Images to verify
-          </header>
-          <div className="Pictures grow shrink flex flex-row gap-2">
-            {imagesToVerify.map((image) => <ImageDnD key={image.id} type="verify" data={image.data} id={image.id} />)}
+          <div className="flex flex-row grow shrink gap-4">
+            { projectUsers.filter((user) => user.role === 'annotator').map((user, index) => (
+              <UserCardDnD key={`${user.id}-annotator`} userId={user.id} accept="annotate" images={toAnnotate.filter((e) => e.user === user.id)} onDrop={(item: any) => handleDrop(index, item, user.id, 'annotate')} />
+            ))}
           </div>
-        </div>
-        <div className="flex grow shrink flex-row gap-4">
-          { projectUsers.filter((user) => user.role === 'verifier').map((user, index) => (
-            <UserCardDnD key={`${user.id}-verifier`} userId={user.id} accept="verify" images={toVerify.filter((e) => e.user === user.id)} onDrop={(item: any) => { handleDrop(index, item, user.id, 'verify'); }} />
-          ))}
         </div>
       </div>
       <br />
-      <button
-        className="bg-black right-2 ml-auto hover:bg-gray-800 text-gray-200 font-bold rounded-full py-1 px-2"
-        type="button"
-        onClick={handleSubmit}
-      >
-        Submit
-      </button>
+      <div id="verification" className="h-1/2 my-5">
+        <h2 className="ml-2 pl-2 mt-3 text-gray-800 text-2xl font-bold ">Verification :</h2>
+        <div className="flex flex-row flex-wrap gap-4 h-full">
+          <div className="flex rounded-lg shadow-xl bg-gray-50">
+            <header className="font-semibold text-sm py-3 px-4">
+              Images to verify
+            </header>
+            <div className="Pictures grow shrink flex flex-row gap-2">
+              {imagesToVerify.map((image) => <ImageDnD key={image.id} type="verify" data={image.data} id={image.id} />)}
+            </div>
+          </div>
+          <div className="flex grow shrink flex-row gap-4">
+            { projectUsers.filter((user) => user.role === 'verifier').map((user, index) => (
+              <UserCardDnD key={`${user.id}-verifier`} userId={user.id} accept="verify" images={toVerify.filter((e) => e.user === user.id)} onDrop={(item: any) => { handleDrop(index, item, user.id, 'verify'); }} />
+            ))}
+          </div>
+        </div>
+      </div>
+      <br />
+      <footer className="flex justify-end pb-8 pt-4">
+
+        <button
+          className="bg-black right-2 ml-auto hover:bg-gray-800 text-gray-200 font-bold rounded-full py-1 px-2"
+          type="button"
+          onClick={handleSubmit}
+        >
+          Assign images
+        </button>
+        <button type="button" id="cancel" onClick={onCancelClick} className="bg-gray-800 text-gray-200 font-bold rounded-full py-1 px-2">
+          Cancel
+        </button>
+      </footer>
     </div>
   );
 }
