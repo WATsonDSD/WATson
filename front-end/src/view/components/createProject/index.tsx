51593afee0a7747136d2f4234b33fff44477e743
ZiedA <almiazied@live.com>
2021-12-12 22:10:36 +0100

    Select annotators and verifiers seperately + once an annotator is selected he doesn't appear in the list when add annotator is clicked

--- front-end/src/view/components/createProject/index.tsx
+++ front-end/src/view/components/createProject/index.tsx
@@ -22,7 +22,8 @@ const templateImage: Image = {
 export default function CreateProject() {
   const [user] = useUserData();
   const allUsers = useData(() => getAllUsers());
-  const [workers, setWorkers] = useState([{ id: 0, worker: '' }]);
+  const [verifiers, setVerifiers] = useState([{ id: 0, worker: '' }]);
+  const [annotators, setAnnotators] = useState([{ id: 0, worker: '' }]);
   const [currentLandMarks, setLandMarks] = useState([] as number[]);
   const [project, setProject] = useState< { name: string, client: string, landmarks: LandmarkSpecification, startDate: Date, endDate: Date, users : UserID[], pricePerImageAnnotation: number,
     pricePerImageVerification: number,
@@ -30,7 +31,6 @@ export default function CreateProject() {
     hourlyRateVerification: number} |null>(null);
   const navigate = useNavigate();
 
-  console.log(workers);
   const handleSubmit = (event: any) => {
     const name = event.target.name.value;
     const client = event.target.client.value;
@@ -42,7 +42,11 @@ export default function CreateProject() {
     const hourlyRateAnnotation = event.target.paymentPerAnn.value;
     const hourlyRateVerification = event.target.paymentPerVer.value;
     const users: UserID[] = [];
-    workers?.forEach((worker) => {
+    annotators?.forEach((worker) => {
+      users.push(worker.worker);
+    });
+
+    verifiers?.forEach((worker) => {
       users.push(worker.worker);
     });
 
@@ -87,9 +91,10 @@ export default function CreateProject() {
   useEffect(() => {
     if (project && user) {
       // the projectManager creating the project is assigned to it
-      createProject(project.name, project.client, project.landmarks, {
-        pricePerImageAnnotation: project.pricePerImageAnnotation, pricePerImageVerification: project.pricePerImageVerification, hourlyRateAnnotation: project.hourlyRateAnnotation, hourlyRateVerification: project.hourlyRateVerification,
-      })
+      createProject(project.name, project.client, project.landmarks,
+        {
+          pricePerImageAnnotation: project.pricePerImageAnnotation, pricePerImageVerification: project.pricePerImageVerification, hourlyRateAnnotation: project.hourlyRateAnnotation, hourlyRateVerification: project.hourlyRateVerification,
+        })
         .then(async (id) => {
           await addUserToProject(user.id, id);
           for (let i = 0; i < project.users.length; i += 1) {
@@ -198,7 +203,7 @@ export default function CreateProject() {
                 </div>
               </div>
 
-              <div className="" style={{ height: '20%' }}>
+              <div className="" style={{ height: '30%' }}>
                 <div className="relative mb-2 flex flex-row">
 
                   <div id="step3" className="w-8 h-8 ml-10 mr-2 bg-white border-2 border-gray-200 rounded-full text-lg text-white flex items-center">
@@ -215,7 +220,7 @@ export default function CreateProject() {
                 </div>
               </div>
 
-              <div className="" style={{ height: '10%' }}>
+              <div className="" style={{ height: '20%' }}>
                 <div className="relative mb-2 flex flex-row">
 
                   <div id="step4" className="w-8 h-8 ml-10 mr-2 bg-white border-2 border-gray-200 rounded-full text-lg text-white flex items-center">
@@ -371,7 +376,7 @@ export default function CreateProject() {
               </div>
             </div>
 
-            <div className="flex flex-wrap -mx-3 mb-2">
+            <div className="flex flex-wrap gap-4 -mx-3 mb-2">
               <div className="w-full">
                 <span className="uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
                   Select workers for the project
@@ -379,17 +384,17 @@ export default function CreateProject() {
               </div>
               <div className="w-full flex flex-col space-x-4 md:w-2/3 px-3 mb-6 md:mb-0">
                 <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-state">
-                  Workers
-                  {workers.map((worker, index) => (
+                  Annotators
+                  {annotators.map((worker, index) => (worker.worker === '' ? (
                     <div className="relative" key={`workers.user${worker.id}`}>
                       <select
                         className="block appearance-none w-full bg-gray-50 border border-gray-50 text-gray-700 py-1 px-2 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                         id={`worker-${index}`}
                         name={`users[${index}].id`}
                         onChange={(e) => {
-                          const newState = Array.from(workers);
+                          const newState = Array.from(annotators);
                           newState[index].worker = e.currentTarget.value;
-                          setWorkers(newState);
+                          setAnnotators(newState);
                         }}
                       >
                         <option value={0}>Select a user</option>
@@ -397,17 +402,72 @@ export default function CreateProject() {
                           .find((w) => w.worker === u.id) === undefined)
                           .map((u) => 
                           (<option value={u.id}>{`${u.name} - ${u.role}`}</option>))} */}
-                        {allUsers?.map((u) => (<option key={u.name} value={u.id}>{`${u.name} - ${u.role}`}</option>))}
+                        {allUsers?.filter((u) => u.role === 'annotator' && !annotators.find((a) => a.worker === u.id)).map((u) => (<option key={u.name} value={u.id}>{`${u.name}`}</option>))}
                       </select>
                       <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                         <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
                       </div>
                     </div>
-                  ))}
+                  ) : (
+                    <div className="relative" key={`workers.user${worker.id}`}>
+                      <input
+                        className="block appearance-none w-full bg-gray-50 border border-gray-50 text-gray-700 py-1 px-2 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                        id={`worker-${index}`}
+                        name={`users[${index}].id`}
+                        type="text"
+                        value={allUsers?.find((u) => u.id === worker.worker)?.name}
+                        readOnly
+                      />
+                    </div>
+                  )))}
                 </label>
 
-                <button type="button" id="btn-add-worker" onClick={() => { setWorkers(workers.concat({ id: workers.length, worker: '' })); }} className=" bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-1 px-2 rounded-l">
-                  Add Worker
+                <button type="button" id="btn-add-worker" onClick={() => { setAnnotators(annotators.concat({ id: annotators.length, worker: '' })); }} className=" bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-1 px-2 rounded-l">
+                  Add Annotator
+                </button>
+              </div>
+              <div className="w-full flex flex-col space-x-4 md:w-2/3 px-3 mb-6 md:mb-0">
+                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-state">
+                  Verifiers
+                  {verifiers.map((worker, index) => (worker.worker === '' ? (
+                    <div className="relative" key={`workers.user${worker.id}`}>
+                      <select
+                        className="block appearance-none w-full bg-gray-50 border border-gray-50 text-gray-700 py-1 px-2 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                        id={`worker-${index}`}
+                        name={`users[${index}].id`}
+                        onChange={(e) => {
+                          const newState = Array.from(verifiers);
+                          newState[index].worker = e.currentTarget.value;
+                          setVerifiers(newState);
+                        }}
+                      >
+                        <option value={0}>Select a user</option>
+                        {/* {allUsers?.filter((u) => workers
+                          .find((w) => w.worker === u.id) === undefined)
+                          .map((u) => 
+                          (<option value={u.id}>{`${u.name} - ${u.role}`}</option>))} */}
+                        {allUsers?.filter((u) => u.role === 'verifier' && !verifiers.find((v) => v.worker === u.id)).map((u) => (<option key={u.name} value={u.id}>{`${u.name} - ${u.role}`}</option>))}
+                      </select>
+                      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                        <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
+                      </div>
+                    </div>
+                  ) : (
+                    <div className="relative" key={`workers.user${worker.id}`}>
+                      <input
+                        className="block appearance-none w-full bg-gray-50 border border-gray-50 text-gray-700 py-1 px-2 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                        id={`worker-${index}`}
+                        name={`users[${index}].id`}
+                        type="text"
+                        value={allUsers?.find((u) => u.id === worker.worker)?.name}
+                        readOnly
+                      />
+                    </div>
+                  )))}
+                </label>
+
+                <button type="button" id="btn-add-worker" onClick={() => { setVerifiers(verifiers.concat({ id: verifiers.length, worker: '' })); }} className=" bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-1 px-2 rounded-l">
+                  Add Verifier
                 </button>
               </div>
               {/* <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
