dccb4751c97828811bdfba8ee97fa71d4f60a6bc
Cem <Cebeci>
2021-12-12 11:58:36 +0100

    removed the unnecessary functions from MockPouch.

--- front-end/src/data/__mocks__/databases.ts
+++ front-end/src/data/__mocks__/databases.ts
@@ -1,30 +1,11 @@
 import { v4 as uuid } from 'uuid';
-import {
-  ImageID, ProjectID, Role, UserID,
-} from '../types';
 
 /* eslint-disable no-underscore-dangle */
 type DBDocument<T> = T & { _id: string, _rev?: string, attach: IMGattachment }
 type IMGattachment = { attachID: string, attachType: string, data: Blob }
-type User = {id: UserID,
-  projects: {
-      [projectID: ProjectID]: {
-          toAnnotate: ImageID[],
-          waitingForAnnotation: ImageID[], // used when the annotation is rejected
-          annotated: ImageID[],
-          toVerify: ImageID[],
-          waitingForVerification: ImageID[], // used when the annotation is rejected and annotated again and 
-                                          // when the annotation is annotated for the first time and is not verified yet.
-          verified: ImageID[],
-      }
-  },
-  name: string,
-  email: string,
-  role: Role}
 
 const MockPouch = <T>() => ({
   documents: {} as { [id: string]: DBDocument<T> },
-  users: {} as { [id: string]: User},
 
   put(document: DBDocument<T>) {
     if (!document._id) { throw Error('document must have an _id'); }
@@ -51,14 +32,6 @@ const MockPouch = <T>() => ({
 
   getAttachment(docId: string) {
     return this.documents[docId].attach.data;
-  },
-
-  putUser(user: User) {
-    if (!user.id) { throw Error('user must have an ID'); }
-    if (this.users[user.id]) {
-      if (this.users[user.id].name !== user.name) { throw Error('User update conflict'); }
-    }
-    this.users[user.id] = { ...user };
   },
 
 });
