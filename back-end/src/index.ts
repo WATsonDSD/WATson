afa0c288b3fe95ed6193b5945199d10b4335726c
Rahman Khalaf <screen.generation.soft@gmail.com>
2021-12-12 14:28:24 +0100

    Untied front-end from back-end

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -19,39 +19,6 @@ server.register(cors, {
   allowedHeaders: ['Origin', 'Accept', 'Content-Type', 'Authorization'],
 });
 
-/**
-   * Fetches and updates the permissions of the
-   * specified 'db' to include the newly created user.
-   * 
-   */
-async function setUserPermissions(db: string, email: string) : Promise<boolean> {
-  return new Promise((resolve, reject) => {
-    cloudant.getSecurity({
-      db: db,
-    }).then(async (security) => {
-      if (security.result) {
-        let updatedSecurity: CloudantV1.JsonObject = {...security.result.cloudant, [email]: ['_reader', '_writer']};
-        
-        await cloudant.putCloudantSecurityConfiguration({
-          db: db,
-          cloudant: updatedSecurity,
-        }).then(() => {
-          resolve(true);
-        });
-      }
-    }).catch((error) => {
-      console.log(error);
-    });
-  });
-}
-
-server.post('/setUserPermissions', async (request, reply) => {
-  const email: string = JSON.parse(JSON.stringify(request.body)).email;
-  const dbs: string[] = ['_users', 'projects', 'images'];
-
-  reply.send((await Promise.all(dbs.map((db) => setUserPermissions(db, email)))).reduce((accumulator, result) => accumulator && result));
-});
-
 server.listen(8080, (error, address) => {
   if (error) {
     console.log(error);
