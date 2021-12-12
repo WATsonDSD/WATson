249c7bc503bd0862ab0a2dbd291133c173d10009
ZiedA <almiazied@live.com>
2021-12-12 22:23:28 +0100

    remove verifier suffix

--- front-end/src/view/components/createProject/index.tsx
+++ front-end/src/view/components/createProject/index.tsx
@@ -446,7 +446,7 @@ export default function CreateProject() {
                           .find((w) => w.worker === u.id) === undefined)
                           .map((u) => 
                           (<option value={u.id}>{`${u.name} - ${u.role}`}</option>))} */}
-                        {allUsers?.filter((u) => u.role === 'verifier' && !verifiers.find((v) => v.worker === u.id)).map((u) => (<option key={u.name} value={u.id}>{`${u.name} - ${u.role}`}</option>))}
+                        {allUsers?.filter((u) => u.role === 'verifier' && !verifiers.find((v) => v.worker === u.id)).map((u) => (<option key={u.name} value={u.id}>{`${u.name}`}</option>))}
                       </select>
                       <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                         <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
