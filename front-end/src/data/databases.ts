bd8fa3a36ac697d9fc4295a25bbc48f274930dad
Cem <Cebeci>
2021-12-13 10:22:33 +0100

    introduced the local db's.

--- front-end/src/data/databases.ts
+++ front-end/src/data/databases.ts
@@ -8,6 +8,16 @@ PouchDB.plugin(PouchDBAuthentication);
 const baseURL = 'https://df6e815f-936a-4c7f-8c66-12c9e8cb2460-bluemix.cloudantnosqldb.appdomain.cloud';
 
 export const AuthDB: PouchDB.Database = new PouchDB(`${baseURL}/_users`, { skip_setup: true, adapter: 'http' });
-export const ImagesDB: PouchDB.Database<Image> = new PouchDB<Image>(`${baseURL}/images`);
-export const ProjectsDB: PouchDB.Database<Project> = new PouchDB<Project>(`${baseURL}/projects`);
-export const rejectionsDB: PouchDB.Database<RejectedAnnotation> = new PouchDB<RejectedAnnotation>(`${baseURL}/rejections`);
+
+const remoteImagesDB: PouchDB.Database<Image> = new PouchDB<Image>(`${baseURL}/images`);
+const remoteProjectsDB: PouchDB.Database<Project> = new PouchDB<Project>(`${baseURL}/projects`);
+const remoteRejectionsDB: PouchDB.Database<RejectedAnnotation> = new PouchDB<RejectedAnnotation>(`${baseURL}/rejections`);
+
+export const ImagesDB: PouchDB.Database<Image> = new PouchDB<Image>('/images');
+ImagesDB.sync(remoteImagesDB, { retry: true });
+
+export const ProjectsDB: PouchDB.Database<Project> = new PouchDB<Project>('/projects');
+ProjectsDB.sync(remoteProjectsDB, { retry: true });
+
+export const rejectionsDB: PouchDB.Database<RejectedAnnotation> = new PouchDB<RejectedAnnotation>('/rejections');
+rejectionsDB.sync(remoteRejectionsDB, { retry: true });
