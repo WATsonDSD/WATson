7893bdd601e66e5cbc6b37e04506b6a3228d8c1b
Cem <Cebeci>
2021-12-13 14:44:50 +0100

    better organization + only buffered db.

--- front-end/src/data/databases.ts
+++ front-end/src/data/databases.ts
@@ -2,15 +2,17 @@ import PouchDB from 'pouchdb';
 import PouchDBAuthentication from 'pouchdb-authentication';
 
 import { Image, Project, RejectedAnnotation } from '.';
-import PouchWrapper from './PouchWrapper';
+import { Buffered, BufferefCached } from './PouchWrapper';
 
 PouchDB.plugin(PouchDBAuthentication);
 
 const baseURL = 'https://df6e815f-936a-4c7f-8c66-12c9e8cb2460-bluemix.cloudantnosqldb.appdomain.cloud';
 
 export const AuthDB: PouchDB.Database = new PouchDB(`${baseURL}/_users`, { skip_setup: true, adapter: 'http' });
-export const ImagesDB: PouchDB.Database<Image> = new PouchDB<Image>(`${baseURL}/images`);
-const projectsDB: PouchDB.Database<Project> = new PouchDB<Project>(`${baseURL}/projects`);
 export const rejectionsDB: PouchDB.Database<RejectedAnnotation> = new PouchDB<RejectedAnnotation>(`${baseURL}/rejections`);
 
-export const ProjectsDB = new PouchWrapper(projectsDB);
+const nonWrappedProjectsDB: PouchDB.Database<Project> = new PouchDB<Project>(`${baseURL}/projects`);
+export const ProjectsDB = new BufferefCached(nonWrappedProjectsDB);
+
+export const nonWrappedImagesDB: PouchDB.Database<Image> = new PouchDB<Image>(`${baseURL}/images`);
+export const ImagesDB = new Buffered(nonWrappedImagesDB);
