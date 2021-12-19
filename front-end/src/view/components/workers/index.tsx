a9201e45adb14ec9403dc381a48ad36b9d947f98
Rahman Khalaf <screen.generation.soft@gmail.com>
2021-12-19 23:49:56 +0100

    Implemented plus button actions.

--- front-end/src/view/components/workers/index.tsx
+++ front-end/src/view/components/workers/index.tsx
@@ -4,19 +4,8 @@ import { getAllUsers } from '../../../data';
 import useData from '../../../data/hooks';
 import Header from '../shared/layout/Header';
 
-// import { useDialog } from '../../../utils/modals';
-// import { CreateUserDialog } from '../shared/dialogs';
-
 export default function Workers() {
   const users = useData(async () => getAllUsers());
-
-  // const dialog = useDialog();
-
-  // const button = (
-  //   <button type="button" onClick={() => dialog.open(<CreateUserDialog onClose={dialog.close} />)} className="bg-transparent hover:bg-gray-400 px-4 py-2 rounded text-black focus:outline-none">
-  //     <AiOutlinePlus />
-  //   </button>
-  // );
 
   const actions = [
     {
