71b14b3a6292e8a232aeeecd3475877920c59cde
Rahman Khalaf <screen.generation.soft@gmail.com>
2021-12-23 23:50:09 +0100

    Getting ready for merge.

--- front-end/src/view/components/createProject/index.tsx
+++ front-end/src/view/components/createProject/index.tsx
@@ -4,6 +4,8 @@ import React, {
   useEffect,
 } from 'react';
 
+// import { v4 } from 'uuid';
+import { useDropzone } from 'react-dropzone';
 import { useNavigate } from 'react-router-dom';
 
 import {
@@ -14,8 +16,6 @@ import {
   Project,
   getAllUsers,
   // createProject,
-  // useUserNotNull,
-  // addUserToProject,
 } from '../../../data';
 
 import useData from '../../../data/hooks';
@@ -23,21 +23,28 @@ import useData from '../../../data/hooks';
 import LandmarksView from './landmarks-view';
 
 import PlusIcon from './plus';
-
 import BackIcon from '../../../assets/icons/back.svg';
 import UploadIcon from '../../../assets/icons/upload.svg';
 
 import { Paths } from '../shared/routes';
 
 export default function CreateProject() {
-  const [project, setProject] = useState<Omit<Project, 'id' | 'status' | 'images' | 'workDoneInTime'>>(
+  const [project, setProject] = useState<Omit<Project, 'id'>>(
     {
       name: '',
       client: '',
-      startDate: new Date(),
-      endDate: new Date(),
+      startDate: new Date(0),
+      endDate: new Date(0),
       landmarks: [],
       users: [],
+      status: 'active',
+      workDoneInTime: {},
+      images: {
+        needsAnnotatorAssignment: [],
+        needsVerifierAssignment: [],
+        pending: [],
+        done: [],
+      },
       hourlyRateAnnotation: 0,
       hourlyRateVerification: 0,
       pricePerImageAnnotation: 0,
@@ -45,26 +52,72 @@ export default function CreateProject() {
     },
   );
 
-  const [selectedLandmarks, updateSelectedLandmarks] = useState<number[]>([]);
+  const [validProject, setProjectValidity] = useState<boolean>(false);
+
+  const [files, setFiles] = useState<File[]>([]);
+
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: 'image/*',
+    onDrop: (acceptedFiles) => {
+      setFiles((previousFiles) => [...previousFiles, ...acceptedFiles]);
+      // TODO: acceptedFiles.map((file) => ({ [v4()]: new Blob([file]) })),
+    },
+  });
+
   const selectedAnnotators = useRef<UserID[]>([]);
   const selectedVerifiers = useRef<UserID[]>([]);
+  const selectedLandmarks = useRef<number[]>([]);
 
   const workers = useData(() => getAllUsers());
+
   const annotators = workers ? workers.filter((user) => user.role === 'annotator') : [];
   const verifiers = workers ? workers.filter((user) => user.role === 'verifier') : [];
 
   const navigate = useNavigate();
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setProject({ ...project, [event.target.name]: event.target.value });
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleValidation = () => {
+    const validGeneralInformation = project.name.length > 0 && project.client.length > 0 && project.endDate > project.startDate;
+    const validLandmarks = project.landmarks.length > 0;
+    const validAssets = files && files.length > 0;
+    const validWorkers = selectedAnnotators.current.length > 0 && selectedVerifiers.current.length > 0;
+    const validPricing = project.hourlyRateAnnotation > 0 && project.hourlyRateVerification > 0 && project.pricePerImageAnnotation > 0 && project.pricePerImageVerification > 0;
+
+    const validateStep = (step: string) => document.getElementById(step)?.classList.add('text-white', 'border-green-500', 'bg-green-500');
+    const invalidateStep = (step: string) => document.getElementById(step)?.classList.remove('text-white', 'border-green-500', 'bg-green-500');
+
+    if (validGeneralInformation) validateStep('step-1');
+    else invalidateStep('step-1');
+
+    if (validLandmarks) validateStep('step-2');
+    else invalidateStep('step-2');
+
+    if (validAssets) validateStep('step-3');
+    else invalidateStep('step-3');
+
+    if (validWorkers) validateStep('step-4');
+    else invalidateStep('step-4');
+
+    if (validPricing) validateStep('step-5');
+    else invalidateStep('step-5');
+
+    setProjectValidity(validGeneralInformation && validLandmarks && validAssets && validWorkers && validPricing);
   };
 
-  const handleSubmit = (event: any) => {
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setProject({ ...project, [event.target.name]: event.target.value.trim() });
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    // const landmarks: LandmarkSpecification = selectedLandmarks;
+
+    console.log(project);
+    // TODO: handle images
   };
 
   useEffect(() => {
+    handleValidation();
     // if (project && user) {
     //   // the projectManager creating the project is assigned to it
     //   createProject(project.name, project.client, project.landmarks, project.startDate, project.endDate, {
@@ -80,22 +133,17 @@ export default function CreateProject() {
     //       navigate(Paths.Projects);
     //     });
     // }
-  }, []);
+  }, [project]);
 
   const range = (startAt: number, endAt: number): number[] => Array.from({ length: endAt - startAt }, (_x, i) => i + startAt);
 
   function handleLandmarksButton(_event: React.MouseEvent<HTMLButtonElement, MouseEvent>, value: number) {
-    const selected = selectedLandmarks.includes(value);
-
-    if (selected) {
-      const newState: number[] = Array.from(selectedLandmarks);
-      newState.splice(newState.indexOf(value), 1);
-      updateSelectedLandmarks(newState);
+    if (selectedLandmarks.current.includes(value)) {
+      selectedLandmarks.current = selectedLandmarks.current.filter((ln) => ln !== value);
     } else {
-      const newState: number[] = Array.from(selectedLandmarks);
-      newState.push(value);
-      updateSelectedLandmarks(newState);
+      selectedLandmarks.current = [...selectedLandmarks.current, value];
     }
+    setProject({ ...project, landmarks: selectedLandmarks.current });
   }
 
   const landmarkButton = (i: number) => (
@@ -104,7 +152,7 @@ export default function CreateProject() {
       type="button"
       onClick={(e) => handleLandmarksButton(e, i)}
       className={`border w-10 h-10 transition-all ${
-        selectedLandmarks.includes(i)
+        selectedLandmarks.current.includes(i)
           ? 'bg-green-200 hover:bg-green-300 border-green-300 text-green-600 hover:text-green-800'
           : 'bg-white hover:bg-gray-100 text-gray-800'
       }`}
@@ -115,19 +163,18 @@ export default function CreateProject() {
 
   function handleLandmarksCheckbox(event: any, values: number[]) {
     if (!event.target.checked) {
-      updateSelectedLandmarks(selectedLandmarks.filter((value) => !values.includes(value)));
+      selectedLandmarks.current = selectedLandmarks.current.filter((value) => !values.includes(value));
     } else {
-      const newState: number[] = Array.from(selectedLandmarks);
-      newState.push(...values);
-      updateSelectedLandmarks(newState);
+      selectedLandmarks.current = [...selectedLandmarks.current, ...values];
     }
+    setProject({ ...project, landmarks: selectedLandmarks.current });
   }
 
   const landmarkCheckbox = (landmarks: number[]) => (
     <input
       type="checkbox"
       className="mr-2"
-      checked={landmarks.every((i) => selectedLandmarks.includes(i))}
+      checked={landmarks.every((i) => selectedLandmarks.current.includes(i))}
       onChange={(event) => handleLandmarksCheckbox(event, landmarks)}
     />
   );
@@ -141,9 +188,14 @@ export default function CreateProject() {
         <div className="flex justify-between">
           <h1 className="self-center text-2xl font-bold">Create a new project</h1>
           <button
-            className="justify-self-end col-start-2 py-2 px-6 border border-gray-400 text-gray-500 text-white rounded-full"
+            id="submit"
+            className={`justify-self-end col-start-2 py-2 px-6 border transition-all rounded-full ${
+              validProject
+                ? 'border-black text-white bg-gray-700 hover:bg-black'
+                : 'border-gray-400 text-gray-500'
+            }`}
             type="button"
-            disabled
+            onClick={() => inputRef?.current?.click()}
           >
             Create the project
           </button>
@@ -151,7 +203,7 @@ export default function CreateProject() {
       </header>
 
       <form id="create-project" onSubmit={handleSubmit}>
-        <span id="step-1" className="flex items-center justify-center w-10 h-10 text-lg border rounded-full">1</span>
+        <span id="step-1" className="flex items-center justify-center w-10 h-10 text-lg border transition-all rounded-full">1</span>
         <div>
           <h3 className="uppercase text-md font-medium">General information</h3>
           <p className="text-gray-400">Provide some general information regarding the scope of this project.</p>
@@ -160,7 +212,7 @@ export default function CreateProject() {
         <div className="grid grid-cols-6 col-start-2 gap-x-8 mb-10">
           <label htmlFor="name" className="flex flex-col items-start col-span-2 text-gray-500">
             Project Name
-            <input id="name" name="name" type="text" placeholder="Jane..." value={project.name} onChange={handleInputChange} required className="w-full py-1 text-black text-lg border-b placeholder-gray-300 focus:outline-none focus:border-black" />
+            <input id="name" name="name" type="text" placeholder="Jane..." value={project.name} onChange={handleInputChange} onBlur={handleValidation} required className="w-full py-1 text-black text-lg border-b placeholder-gray-300 focus:outline-none focus:border-black" />
           </label>
           <label htmlFor="client" className="flex flex-col items-start col-span-2 text-gray-500">
             Client
@@ -176,7 +228,7 @@ export default function CreateProject() {
           </label>
         </div>
 
-        <span id="step-2" className="flex items-center justify-center w-10 h-10 text-lg border rounded-full">2</span>
+        <span id="step-2" className="flex items-center justify-center w-10 h-10 text-lg border transition-all rounded-full">2</span>
         <div>
           <h3 className="uppercase text-md font-medium">Landmarks</h3>
           <p className="text-gray-400">Select the specific face features that the workers will have to annotate in the project</p>
@@ -184,7 +236,7 @@ export default function CreateProject() {
 
         <div className="grid grid-cols-6 items-center col-start-2 gap-x-20 2xl:gap-0 mt-4 mb-8">
           <div className="col-span-3">
-            <div className="w-full 2xl:w-4/5 p-16 border rounded-xl"><LandmarksView selectedLandmarks={selectedLandmarks} /></div>
+            <div className="w-full 2xl:w-4/5 p-16 border rounded-xl"><LandmarksView selectedLandmarks={selectedLandmarks.current} /></div>
           </div>
 
           <div className="flex flex-col gap-y-6 col-span-2">
@@ -278,59 +330,69 @@ export default function CreateProject() {
           </div>
         </div>
 
-        <span id="step-3" className="flex items-center justify-center w-10 h-10 text-lg border rounded-full">3</span>
+        <span id="step-3" className="flex items-center justify-center w-10 h-10 text-lg border transition-all rounded-full">3</span>
         <div>
           <h3 className="uppercase text-md font-medium">Upload assets</h3>
           <p className="text-gray-400">Select the images/videos that will be uploaded as part of this project</p>
         </div>
 
         <div className="flex flex-col gap-y-8 col-start-2 mb-8">
-          <button type="button" className="flex flex-col items-center justify-center gap-y-2 py-8 w-full border border-blue-200 hover:border-blue-300 bg-blue-50 text-blue-300 hover:text-blue-400 transition-all rounded-md">
+          { /* eslint-disable react/jsx-props-no-spreading */ }
+          <div {...getRootProps({ className: 'dropzone cursor-pointer flex flex-col items-center justify-center gap-y-2 py-8 w-full border border-blue-200 hover:border-blue-300 bg-blue-50 text-blue-300 hover:text-blue-400 transition-all rounded-md' })}>
+            <input {...getInputProps()} />
             <img src={UploadIcon} alt="Upload media" />
             Drag and drop, or click to upload.
-          </button>
+          </div>
 
           <div className="w-full rounded-lg border shadow-sm">
             <div className="flex items-center justify-between mx-6 py-6 border-b">
-              <h3 className="text-lg font-medium">Uploaded assets</h3>
-              <button type="button" className="px-6 py-1 border border-red-200 hover:bg-red-50 hover:border-red-300 hover:text-red-500 text-red-400 transition-all rounded-full">Remove all</button>
+              <h3 className="text-lg font-medium py-1">Uploaded assets</h3>
+              {files && files.length > 0 ? (
+                <button
+                  type="button"
+                  onClick={() => setFiles([])}
+                  className="px-6 py-1 border border-red-200 hover:bg-red-50 hover:border-red-300 hover:text-red-500 text-red-400 transition-all rounded-full"
+                >
+                  Remove all
+                </button>
+              ) : ''}
             </div>
 
-            <div className="flex flex-col">
-              <div className="flex gap-x-4 mx-6 mb-2 py-2 text-sm text-gray-500 border-b">
+            <div className="flex flex-col gap-y-4 mb-4">
+              <div className="flex gap-x-4 mx-6 py-2 text-sm text-gray-500 border-b">
                 <span className="w-full">File name</span>
                 <span className="w-1/6">Type</span>
                 <span className="w-1/6">Extension</span>
                 <span className="w-1/12 text-center">Size</span>
               </div>
 
-              <div className="flex gap-x-4 px-6 py-3 text-gray-800">
-                <span className="w-full">IMG_9336</span>
-                <span className="w-1/6">Image</span>
-                <span className="w-1/6">JPEG</span>
-                <span className="w-1/12 text-center">701KB</span>
-              </div>
-              <div className="flex gap-x-4 px-6 py-3 text-gray-800">
-                <span className="w-full">IMG_9336</span>
-                <span className="w-1/6">Image</span>
-                <span className="w-1/6">JPEG</span>
-                <span className="w-1/12 text-center">701KB</span>
-              </div>
-              <div className="flex gap-x-4 px-6 py-3 text-gray-800">
-                <span className="w-full">IMG_9336</span>
-                <span className="w-1/6">Image</span>
-                <span className="w-1/6">JPEG</span>
-                <span className="w-1/12 text-center">701KB</span>
-              </div>
+              {files && files.length > 0 ? files.slice(0, 5).map((file) => (
+                <div key={file.name} className="flex gap-x-4 px-6 text-gray-800">
+                  <span className="w-full">{file.name}</span>
+                  <span className="capitalize w-1/6">{file.type.split('/')[0]}</span>
+                  <span className="uppercase w-1/6">{file.type.split('/')[1]}</span>
+                  <span className="w-1/12 text-center">{file.size}</span>
+                </div>
+              )) : (
+                <div className="flex justify-center text-gray-400">
+                  You didn&apos;t upload any assets yet.
+                </div>
+              )}
 
-              <div className="flex mt-2 px-6 py-3 bg-gray-50">
-                <span>+997 other files</span>
+              {files && files.length > 5 && (
+              <div className="flex bg-gray-50">
+                <span>
+                  {files.length - 5}
+                  {' '}
+                  other files
+                </span>
               </div>
+              )}
             </div>
           </div>
         </div>
 
-        <span id="step-4" className="flex items-center justify-center w-10 h-10 text-lg border rounded-full">4</span>
+        <span id="step-4" className="flex items-center justify-center w-10 h-10 text-lg border transition-all rounded-full">4</span>
         <div>
           <h3 className="uppercase text-md font-medium">Workers</h3>
           <p className="text-gray-400">Choose the users that you want to include as workers in this project</p>
@@ -351,7 +413,7 @@ export default function CreateProject() {
                     }
                     setProject({ ...project, users: [...selectedAnnotators.current, ...selectedVerifiers.current] });
                   }}
-                  className={`px-6 py-1 border  transition-all rounded-full ${
+                  className={`px-6 py-1 border transition-all rounded-full ${
                     selectedAnnotators.current.length === annotators.length
                       ? 'border-red-200 hover:bg-red-50 hover:border-red-300 hover:text-red-500 text-red-400'
                       : 'border-green-200 hover:bg-green-50 hover:border-green-300 hover:text-green-500 text-green-400'
@@ -388,7 +450,7 @@ export default function CreateProject() {
                     role="listitem"
                     onClick={handleClick}
                     onKeyDown={handleClick}
-                    className={`grid grid-cols-8 items-center px-6 py-3 cursor-pointer ${isEven ? 'bg-gray-50' : ''} transition-all ${included ? 'text-black bg-blue-50' : 'text-gray-400 hover:text-gray-500'}`}
+                    className={`grid grid-cols-8 items-center px-6 py-3 cursor-pointer ${isEven ? 'bg-gray-50' : ''} transition-all ${included ? 'text-black' : 'text-gray-400 hover:text-gray-500'}`}
                   >
                     <span className="col-span-2">{user.name}</span>
                     <span className="col-span-5">{user.email}</span>
@@ -466,7 +528,7 @@ export default function CreateProject() {
                     role="listitem"
                     onClick={handleClick}
                     onKeyDown={handleClick}
-                    className={`grid grid-cols-8 items-center px-6 py-3 cursor-pointer ${isEven ? 'bg-gray-50' : ''} transition-all ${included ? 'text-black bg-blue-50' : 'text-gray-400 hover:text-gray-500'}`}
+                    className={`grid grid-cols-8 items-center px-6 py-3 cursor-pointer ${isEven ? 'bg-gray-50' : ''} transition-all ${included ? 'text-black' : 'text-gray-400 hover:text-gray-500'}`}
                   >
                     <span className="col-span-2">{user.name}</span>
                     <span className="col-span-5">{user.email}</span>
@@ -494,7 +556,7 @@ export default function CreateProject() {
           </div>
         </div>
 
-        <span id="step-5" className="flex items-center justify-center w-10 h-10 text-lg border rounded-full">5</span>
+        <span id="step-5" className="flex items-center justify-center w-10 h-10 text-lg border transition-all rounded-full">5</span>
         <div>
           <h3 className="uppercase text-md font-medium">Pricing model</h3>
           <p className="text-gray-400">Determine the price of the work done by the annotators and the verifiers in the project</p>
@@ -530,6 +592,7 @@ export default function CreateProject() {
             </span>
           </label>
         </div>
+        <input ref={inputRef} className="hidden" type="submit" />
       </form>
     </>
   );
