7ffe417da6a9716ce6e7d19e2d4a33d29279de9a
LauraAmabili <laura.amabili@mail.polimi.it>
2021-12-30 14:58:28 +0100

    left images

--- front-end/src/view/components/project/ProjectAssign.tsx
+++ front-end/src/view/components/project/ProjectAssign.tsx
@@ -128,8 +128,12 @@ export default function ProjectAssign() {
         <div className="w-full flex flex-row space-x-4 md:w-2/3 px-3 mb-6 md:mb-0">
           <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-state">
             Number Of Images
+            {' '}
             <div className="relative">
               <input required className="appearance-none block w-full bg-gray-50 text-gray-700 border border-gray-50 rounded py-1 px-2 leading-tight" id="numberImages" name="numberImages" type="number" />
+              LEFT:
+              {' '}
+              {project?.images.imagesWithoutAnnotator.length}
             </div>
           </label>
           <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-state">
