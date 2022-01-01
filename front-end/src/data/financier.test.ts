0bc79a2950946555bdc406295d19822b85dc5c62
LauraAmabili <laura.amabili@mail.polimi.it>
2022-01-01 20:08:00 +0100

    fixed test + editProject

--- front-end/src/data/financier.test.ts
+++ front-end/src/data/financier.test.ts
@@ -150,10 +150,10 @@ describe('adding verification', () => {
     expect(data[1]).toBe(7.5);
   }));
   test('chart december', () => dataChartWorker(annotatorId).then((data) => {
-    expect(data[11]).toBe(30);
+    expect(data[11]).toBe(0);
   }));
   test('chart january', () => dataChartWorker(annotatorId).then((data) => {
-    expect(data[0]).toBe(0);
+    expect(data[0]).toBe(30);
   }));
   test('hours work', () => hoursWorkPerProjectPerUser(annotatorId, projectId).then((data) => {
     expect(data).toBe(7.5);
@@ -165,7 +165,7 @@ describe('adding verification', () => {
     expect(data).toBe(69);
   }));
   test('chart project', () => dataChartProjects(projectId).then((data) => {
-    expect(data[11]).toBe(99);
+    expect(data[0]).toBe(99);
   }));
   test('percentage', () => percentageOfImagesDone(projectId).then((data) => {
     expect(data).toBe(0.75);
