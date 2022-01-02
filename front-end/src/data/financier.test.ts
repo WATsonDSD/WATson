39790990b79e46c8881d026bcf1941e1b2828d23
LauraAmabili <laura.amabili@mail.polimi.it>
2022-01-02 16:23:20 +0100

    fixed test

--- front-end/src/data/financier.test.ts
+++ front-end/src/data/financier.test.ts
@@ -126,10 +126,10 @@ describe('adding verification', () => {
     expect(data[1]).toBe(7.5);
   }));
   test('chart december', () => dataChartWorker(userId).then((data) => {
-    expect(data[11]).toBe(30);
+    expect(data[11]).toBe(0);
   }));
   test('chart january', () => dataChartWorker(userId).then((data) => {
-    expect(data[0]).toBe(0);
+    expect(data[0]).toBe(30);
   }));
   test('hours work', () => hoursWorkPerProjectPerUser(userId, projectId).then((data) => {
     expect(data).toBe(7.5);
@@ -141,6 +141,6 @@ describe('adding verification', () => {
     expect(data).toBe(69);
   }));
   test('chart project', () => dataChartProjects(projectId).then((data) => {
-    expect(data[11]).toBe(99);
+    expect(data[0]).toBe(99);
   }));
 });
