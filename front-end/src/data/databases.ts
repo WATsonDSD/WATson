c982dfb08fbf08f658ae4fffe74a3076ac4996bf
Cem <Cebeci>
2022-01-04 12:59:15 +0100

    experimenting with cached db's

--- front-end/src/data/databases.ts
+++ front-end/src/data/databases.ts
@@ -9,11 +9,23 @@ import { Buffered, BufferefCached } from './PouchWrapper';
 PouchDB.plugin(PouchDBAuthentication);
 
 const baseURL = 'https://watson.visagetechnologies.com/couchdb';
+const syncOptions = {
+  live: true,
+  retry: true,
+};
 
 export const AuthDB: PouchDB.Database = new PouchDB(`${baseURL}/_users`, { skip_setup: true, adapter: 'http' });
 export const RejectionsDB: PouchDB.Database<RejectedAnnotation> = new PouchDB<RejectedAnnotation>(`${baseURL}/rejections`);
 
-const nonWrappedProjectsDB: PouchDB.Database<Project> = new PouchDB<Project>(`${baseURL}/projects`);
+const remoteProjectDB: PouchDB.Database<Project> = new PouchDB<Project>(`${baseURL}/projects`);
+const nonWrappedProjectsDB: PouchDB.Database<Project> = new PouchDB<Project>('projects');
+nonWrappedProjectsDB.sync(remoteProjectDB, syncOptions).on('change', (change) => {
+  console.log('replicating change: ', change);
+}).on('paused', (info) => {
+  console.log('paused replication: ', info);
+}).on('error', (err) => {
+  console.log('error on replicaiton:', err);
+});
 export const ProjectsDB = new BufferefCached(nonWrappedProjectsDB);
 
 export const nonWrappedImagesDB: PouchDB.Database<Image> = new PouchDB<Image>(`${baseURL}/images`);
