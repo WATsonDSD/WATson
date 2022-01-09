383fbd04e6ecab2a7898a47a4bca4f185e083d08
Rahman Khalaf <screen.generation.soft@gmail.com>
2022-01-09 12:01:59 +0100

    Added dropdown component

--- front-end/src/view/components/workers/Dropdown.tsx
+++ front-end/src/view/components/workers/Dropdown.tsx
@@ -5,16 +5,50 @@ import {
   Transition,
 } from '@headlessui/react';
 
+import { Worker } from '../../../data';
+
 function classNames(...classes: any) {
   return classes.filter(Boolean).join(' ');
 }
 
-const Dropdown = (props: {elements: ReactElement[], icon: ReactElement}) => {
-  const { elements, icon } = props;
+const Dropdown = (props: {user: Worker, icon: ReactElement}) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { user, icon } = props;
+
   let counter = 0;
 
+  const options = [
+    {
+      name: 'Edit Worker',
+      action: () => {}, // TODO: implement update user data dialog
+    },
+    {
+      name: 'Award Bonus',
+      action: () => {},
+    },
+    {
+      name: 'Delete User',
+      action: () => {}, // TODO: implement delete user dialog
+    },
+  ];
+
+  const dropDownOptions = options.map((option: {name: string, action: Function}) => ((
+    <button
+      id={`${option.name}-btn`}
+      className="whitespace-nowrap"
+      type="button"
+      onClick={(event) => {
+        event.stopPropagation();
+        option.action();
+      }}
+    >
+      {option.name}
+    </button>
+  )
+  ));
+
   return (
-    <div className="Dropdown">
+    <div className="Dropdown justify-self-center">
       <Menu as="div" className="z-10 relative text-left my-0.5 max-h-6">
         <Menu.Button>
           <span className="my-1 block w-auto font-bold px-2 align-middle text-center">{icon}</span>
@@ -29,9 +63,9 @@ const Dropdown = (props: {elements: ReactElement[], icon: ReactElement}) => {
           leaveFrom="transform opacity-100 scale-100"
           leaveTo="transform opacity-0 scale-95"
         >
-          <Menu.Items className="absolute right-3/4 bg-ui-darkgray rounded-md shadow-lg py-2 focus:outline-none divide-y">
+          <Menu.Items className="absolute right-3/4 bg-white rounded-md shadow-lg py-2 focus:outline-none">
             <div>
-              {elements?.slice(0, -1).map((element : ReactElement) => {
+              {dropDownOptions.slice(0, -1).map((option) => {
                 counter += 1;
                 return (
                   <Menu.Item key={counter}>
@@ -39,11 +73,11 @@ const Dropdown = (props: {elements: ReactElement[], icon: ReactElement}) => {
                       <span
                         key={counter}
                         className={classNames(
-                          active ? 'bg-gray-700 text-white' : 'text-white',
+                          active ? 'text-black' : 'text-gray-600',
                           'block pl-6 pr-12 py-2 text-base font-medium normal-case',
                         )}
                       >
-                        {element}
+                        {option}
                       </span>
                     )}
                   </Menu.Item>
@@ -55,11 +89,11 @@ const Dropdown = (props: {elements: ReactElement[], icon: ReactElement}) => {
                 <span
                   key={counter}
                   className={classNames(
-                    active ? 'bg-gray-700 ' : '',
-                    'text-ui-darkred block pl-6 pr-12 py-2 text-base font-medium normal-case',
+                    active ? 'text-red-800 ' : 'text-red-600',
+                    'block pl-6 pr-12 py-2 text-base font-medium normal-case',
                   )}
                 >
-                  {elements[elements.length - 1]}
+                  {dropDownOptions[dropDownOptions.length - 1]}
                 </span>
               )}
             </Menu.Item>
