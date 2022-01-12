0252517f8e9723cf09b08978227592eecee60abe
Rahman Khalaf <screen.generation.soft@gmail.com>
2022-01-12 10:19:29 +0100

    Added 404 page

--- front-end/src/view/components/404/index.tsx
+++ front-end/src/view/components/404/index.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
 
+import { useNavigate, Link } from 'react-router-dom';
+
+import { Paths } from '../shared/routes/paths';
+
+import logo from '../../logo.svg';
+
 export default function NotFound() {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      Not found
+    <div id="not-found" className="flex justify-between px-24 py-20">
+      <div className="w-full">
+        <Link to={Paths.Projects}>
+          <img src={logo} alt="Logo" />
+        </Link>
+        <div className="flex flex-col mt-64">
+          <span className="text-8xl">404</span>
+          <span className="text-xl font-medium">Something went wrong, we couldn&apos;t find the page you were looking for :(</span>
+        </div>
+        <button type="button" onClick={() => navigate(Paths.Projects)} className="max-w-content mt-10 px-6 py-2 border border-gray-800 rounded-full">
+          Back to home
+        </button>
+      </div>
+      <div className="flex justify-end items-center w-full pr-16" />
     </div>
   );
 }
